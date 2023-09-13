c43b99df6b87055cee2bc3cbac65e45d3e828b40
Lynxtail <l.gorbachev@mail.ru>
2023-09-13 17:37:22 +0400

    fix next, add list of clans

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,5 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 menu = [
     [InlineKeyboardButton(text='Мои персонажи', callback_data='list_of_chars'),
@@ -12,12 +12,15 @@ menu = InlineKeyboardMarkup(inline_keyboard=menu)
 exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='В меню')]], resize_keyboard=True)
 iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='В меню', callback_data='menu')]])
 
-# def next(step:int):
-#     return InlineKeyboardMarkup(inline_keyboard=InlineKeyboardButton(text='Далее', callback_data=f'{step}'))
+previous_step = InlineKeyboardButton(text='Назад', callback_data='back')
+next_step = InlineKeyboardButton(text='Далее', callback_data='next')
+done = InlineKeyboardButton(text='Завершить', callback_data='done')
+break_creation = InlineKeyboardButton(text='Главное меню', callback_data='break')
 
-# next_step = InlineKeyboardMarkup(inline_keyboard=InlineKeyboardButton(text='Далее', callback_data='next'))
-next_step = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Мои персонажи', callback_data='list_of_chars')]])
-# done = InlineKeyboardMarkup(inline_keyboard=InlineKeyboardButton(text='Завершить', callback_data='done'))
+welcome_creation = InlineKeyboardMarkup(inline_keyboard=[[previous_step, next_step]])
+next_step_creation = InlineKeyboardMarkup(inline_keyboard=[[previous_step, next_step], [break_creation]])
+done_creation = InlineKeyboardMarkup(inline_keyboard=[[previous_step, done], [break_creation]])
+
 
 # для динамических клавиатур
 def list_of_chars(characters: dict):
@@ -26,6 +29,17 @@ def list_of_chars(characters: dict):
         builder.button(text=f'{item.name}', callback_data=f"char_{i}")
     builder.adjust(2)
     return builder.as_markup()
+
+def list_of_clans():
+    clans = ("Бруха", "Гангрел", "Малкавиан", "Тореадор", 
+            "Носферату", "Вентру", "Тремер", "Цимисх",
+            "Ласомбра", "Равнос", "Бану Хаким", "Каитифф",
+            "Слабая Кровь", "Салюбри", "Министри")
+    builder = ReplyKeyboardBuilder()
+    for item in clans:
+        builder.button(text=item)
+    builder.adjust(4)
+    return builder.as_markup(one_time_keyboard=True)
 
 def creation_steps():
     builder = InlineKeyboardBuilder()
